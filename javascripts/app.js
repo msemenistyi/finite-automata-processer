9647847e6b581315527321481967e66783e2294c
Mykyta Semenistyi <nikeiwe@gmail.com>
2013-04-13 23:51:09 +0300

    refactored KissFormat: introduced KissGrapics class and decoupled them with Mediator.js

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,10 @@
 $(function(){
 	var container = $("#input-form");
-
 	var canvas = $("#canvas");
-
 	canvas.attr({width: $(window).innerWidth() * 0.98});
 
-	kissFormat = new KissFormat({container: container, canvas: canvas});
+	var mediator = new Mediator;
+
+	var kissGraphics = new KissGraphics({canvas: canvas, mediator: mediator});
+	var kissFormat = new KissFormat({container: container, mediator: mediator});
 });
